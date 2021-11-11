b775dae798d0d250a386c91b5b7e08a6a08ad031
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2021-11-11 16:53:14 +0100

    assign function launch

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ let teamListNode = document.querySelector(".team-list")
 
 // Array of Names and Teams
 let namesArr = []
-let teamArr = []
+let teamNumArr = []
+
+
 
 const addNamesList = function() {
     let nameValue = nameInputNode.value
@@ -14,7 +16,11 @@ const addNamesList = function() {
     if(nameValue !== " ") {
 
     let newNameList = document.createElement('li')
+    let deleteIcon = document.createElement('i')
+
+    deleteIcon.innerHTML = '<i class="far fa-trash-alt"></i>'
     newNameList.innerText = nameValue
+    newNameList.appendChild(deleteIcon)
     nameListNode.appendChild(newNameList)
     nameInputNode.value = " "
     namesArr.push(nameValue)
@@ -22,13 +28,29 @@ const addNamesList = function() {
     }
 }
 
+
+
 const takeNumTeams = function() {
     let teamNumberValue = teamNumberNode.value
     
     for (let i = 1; i <= teamNumberValue; i++) {
+
         let teamHeader = document.createElement('p')
         teamHeader.innerText = `Team ${i}`
         teamListNode.appendChild(teamHeader)
         teamNumberNode.value = " "
+
+        teamNumArr.push(`team${i}`)
+        console.log(teamNumArr)
+
     }
+    return teamNumberValue
+}
+
+
+const assignTeams = function() {
+    let oneName = document.querySelectorAll('.name-list li')
+    let randomNumName = Math.floor(Math.random() * namesArr.length)
+    console.log(oneName)
+
 }
